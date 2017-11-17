ea180e714a5bacd10f847c510be014da99e98f9a
M.C. van den Boogaart <scola@kvdb.name>
2017-11-17 12:45:08 +0100

    Initial import

---
+++ index.js
@@ -0,0 +1,29 @@
+import get from 'lodash-es/get';
+import sprintf from 'sprintf-js';
+
+let locale = null;
+let stringFormat = null;
+
+function stringFormatDefaultLocale(definition) {
+  locale = stringFormatLocale(definition);
+  stringFormat = locale.format;
+}
+
+function stringFormatLocale(definition) {
+  return {
+    format(prefix = null) {
+      return (code, ...args) => {
+        code = prefix ? prefix + '.' + code : code;
+        return sprintf.vsprintf(get(definition, code) || code, args);
+      };
+    }
+  };
+}
+
+stringFormatDefaultLocale({});
+
+export {
+  stringFormat,
+  stringFormatDefaultLocale,
+  stringFormatLocale
+};
