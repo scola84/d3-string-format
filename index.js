ac011f2d29b6aa2eb7290abfdd0efbd664f898a3
M.C. van den Boogaart <scola@kvdb.name>
2018-02-17 10:07:53 +0100

    Add date and markdown

--- index.js
+++ index.js
@@ -1,33 +1,88 @@
+import { DateTime } from 'luxon';
+import marked from 'marked';
 import get from 'lodash-es/get';
 import sprintf from 'sprintf-js';
 
 let locale = null;
 let stringFormat = null;
 
-function stringFormatDefaultLocale(definition) {
-  locale = stringFormatLocale(definition);
+function stringFormatDefaultLocale(definition, name = 'en_GB') {
+  locale = stringFormatLocale(definition, name);
   stringFormat = locale.format;
 }
 
-function stringFormatLocale(definition) {
+function stringFormatLocale(definition, name) {
   return {
     format(prefix = null) {
       return (code, ...args) => {
-        let string = get(definition,
+        let value = get(definition,
           prefix ? prefix + '.' + code : code);
 
-        if (Array.isArray(string)) {
+        if (Array.isArray(value)) {
           args = String(args[0]).split(' ');
-          string = string[args.length - 1];
-        } else if (typeof string === 'object') {
-          string = args[0] && string[args[0]] ||
-            string[args[0].count] || string.d;
+          value = value[args.length - 1];
+        } else if (typeof value === 'object') {
+          value = args[0] && (value[args[0]] ||
+            value[args[0].count] || value.v) || value.d;
+        } else if (typeof value === 'function') {
+          value = value(...args);
         }
 
-        return sprintf.vsprintf(string, args);
+        const md = value && value.match(/(%md)/);
+
+        if (md) {
+          value = formatMarkdown(args, value, md);
+        }
+
+        const date = value && value.match(/%\(([a-z ]*)+\)ds/gi);
+
+        if (date) {
+          value = formatDate(args, value, date, name);
+        }
+
+        return typeof value === 'string' ?
+          sprintf.vsprintf(value, args) :
+          value;
       };
     }
   };
+}
+
+function formatMarkdown(args, value, md) {
+  return value.replace(md[1], marked(args[0], {
+    breaks: true,
+    sanitize: true
+  }));
+}
+
+function formatDate(args, value, date, name) {
+  let format = null;
+  let offset = null;
+  let sign = null;
+  let time = null;
+
+  for (let i = 0; i < date.length; i += 1) {
+    format = date[i].slice(2, -3);
+
+    if (!args[i]) {
+      value = value.replace(date[i], '');
+      continue;
+    }
+
+    [, time, sign, offset] = String(args[i])
+      .match(/(\d*)([-+]?)(\d*)/);
+
+    sign = sign || '+';
+    time = Number(time);
+    offset = offset || 0;
+
+    value = value.replace(date[i], DateTime
+      .fromMillis(time, { zone: 'UTC' + sign + (offset / 3600) })
+      .setLocale(name.replace('_', '-'))
+      .toFormat(format));
+  }
+
+  return value;
 }
 
 stringFormatDefaultLocale({});
