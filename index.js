27e1eb258b47721e76e4afa21996154ec6ad0639
M.C. van den Boogaart <scola@kvdb.name>
2018-08-20 12:40:33 +0200

    Parse object strings too

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import bytes from 'bytes';
 import { format as numberFormat } from 'd3';
 import { DateTime } from 'luxon';
 import get from 'lodash-es/get';
-import findKey from 'lodash-es/findKey';
 import marked from 'marked';
 import sprintf from 'sprintf-js';
 
@@ -81,9 +80,34 @@ function stringFormatLocale(definition) {
       };
     },
     parse(prefix = null) {
+      function findKey(object, pre, cb) {
+        const keys = Object.keys(object);
+
+        let found = null;
+        let full = null;
+        let key = null;
+
+        for (let i = 0; i < keys.length; i += 1) {
+          key = keys[i];
+          full = [pre, key].filter((v) => v).join('.');
+
+          if (typeof object[key] === 'object') {
+            found = findKey(object[key], full, cb);
+          } else {
+            found = cb(object[key]) ? full : null;
+          }
+
+          if (found) {
+            return found;
+          }
+        }
+
+        return object[-1];
+      }
+
       return (base, value = '') => {
         const object = get(definition, prefix ? prefix + '.' + base : base);
-        return findKey(object, (v) => {
+        return findKey(object, '', (v) => {
           return String(v).toLowerCase() === String(value).toLowerCase();
         });
       };
